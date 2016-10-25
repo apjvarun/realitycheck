f3de955559357f5a52ecb8bab6fae4ab545881fd
= <=>
2016-10-25 18:05:02 +0530

    extract feature matrix using this file

--- matlabPyrTools/run_cgorphoto.m
+++ matlabPyrTools/run_cgorphoto.m
@@ -1,3 +1,7 @@
+%% This file extracts feature matrix and saves it as ftr.mat
+% The input set contains 800 positive class and 800 negative class.
+
+
 clear all
 
 itr = 1;
@@ -24,39 +28,4 @@ for file = files'
     % Do some stuff
 end
 
-feature_matrix = ftr;
-
-%%
-k_list = 700:10:800;
-acc = zeros(length(k_list),1);
-index = 1;
-for k = k_list
-    
-    ftr1 = feature_matrix([1:k, 801:(800+k)], :);
-    all = randperm(size(ftr1,1));
-    train = all(1:round(0.8*size(ftr1,1)));
-    test = all(round(0.8*size(ftr1,1))+1:end);
-    
-    
-    X_train = ftr1(train,1:216);
-    Y_train = ftr1(train,217);
-    X_test = ftr1(test,1:216);
-    Y_test = ftr1(test,217);
-    
-    
-    %% Training
-    
-    SVMStruct = svmtrain(X_train,Y_train, 'Kernel_Function','polynomial');
-    
-    %% Testing
-    
-    pred = svmclassify(SVMStruct,X_test);
-    
-    
-    %% Evaluation
-    acc(index) = sum(pred == Y_test)/length(Y_test)*100
-    index = index+1;
-    
-end
-
-plot(k_list, acc);
+save(ftr.mat, ftr); 
