7421ea0be627ad42418c5245d1093bd031e7aa33
Varun Gupta <varunapj1@gmail.com>
2016-10-25 20:57:28 +0530

    Update SVMacc_iter.m

--- SVMacc_iter.m
+++ SVMacc_iter.m
@@ -1,28 +1,5 @@
-clear all
-
-itr = 1;
-
-% For positive class
-files = dir('../columbia-prcg-datasets/google_images/*.jpg');
-% files = dir('../columbia-prcg-datasets/goog_test/*.jpg')
-
-dir_addr = '../columbia-prcg-datasets/google_images/'
-for file = files'
-    im = imread(strcat(dir_addr, file.name));
-    ftr(itr,:) = [cgorphoto( im ), 1];
-    itr = itr+1
-    % Do some stuff
-end
-
-% For negative class
-files = dir('../columbia-prcg-datasets/prcg_images/*.jpg')
-dir_addr = '../columbia-prcg-datasets/prcg_images/'
-for file = files'
-    im = imread(strcat(dir_addr, file.name));
-    ftr(itr,:) = [cgorphoto( im ), 0];
-    itr = itr+1
-    % Do some stuff
-end
+clear all;
+load ftr.mat;
 
 feature_matrix = ftr;
 
